39b155ed4729147fc54fa7d25ece3f9b16640839
Sacha <andryushin.alexander@gmail.com>
2020-08-31 13:12:46 +0700

    ex 2.1

--- exercises/chapter2/tempconv/conv.go
+++ exercises/chapter2/tempconv/conv.go
@@ -7,3 +7,15 @@ func CToF(c Celsius) Fahrenheit {
 func FToC(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
 }
+
+func KToC(k Kelvin) Celsius {
+	return Celsius(k - 273.15)
+}
+
+func KToF(k Kelvin) Fahrenheit {
+	return Fahrenheit(k*(9/5) - 459.67)
+}
+
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c + 273.15)
+}
