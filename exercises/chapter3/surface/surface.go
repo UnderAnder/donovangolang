4b6aaaac3f32703795f2ba6dcda0abecf9ae63d0
Sacha <andryushin.alexander@gmail.com>
2020-09-01 19:58:50 +0700

    ex 3.4

--- exercises/chapter3/surface/surface.go
+++ exercises/chapter3/surface/surface.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"math"
 )
 
 const (
-	width, height = 600, 320
+	width, height = 640, 420
 	cells         = 100
 	xyrange       = 30.0
 	xyscale       = width / 2 / xyrange
@@ -16,35 +17,29 @@ const (
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
-func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+func plotSVG(w io.Writer, width, height int) {
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			//ax, ay := corner(i+1, j)
-			//bx, by := corner(i, j)
-			//cx, cy := corner(i, j+1)
-			//dx, dy := corner(i+1, j+1)
-			//fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g'/>\n",
-			//	ax, ay, bx, by, cx, cy, dx, dy)
 			ax, ay, _ := corner(i+1, j)
 			bx, by, z := corner(i, j)
 			cx, cy, _ := corner(i, j+1)
 			dx, dy, _ := corner(i+1, j+1)
 
 			if z >= 0 {
-				fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' "+
+				fmt.Fprintf(w, "<polygon points='%g,%g %g,%g %g,%g %g,%g' "+
 					"fill='rgb(%2.1f%%,0%%,0%%)'/>\n", //#%02x00%02x rgb(%02d%%,00%%,%02d%%)
 					ax, ay, bx, by, cx, cy, dx, dy, z*100)
 			} else {
-				fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' "+
+				fmt.Fprintf(w, "<polygon points='%g,%g %g,%g %g,%g %g,%g' "+
 					"fill='rgb(0%%,0%%,%2.1f%%)'/>\n", //#%02x00%02x rgb(%02d%%,00%%,%02d%%)
 					ax, ay, bx, by, cx, cy, dx, dy, z*-100)
 			}
 		}
 	}
-	fmt.Println("</svg>")
+	fmt.Fprint(w, "</svg>")
 }
 
 func corner(i, j int) (float64, float64, float64) {
