fa86388917a2e7edba43180893832d87eefca15d
Sacha <andryushin.alexander@gmail.com>
2020-09-03 11:41:00 +0700

    ex 3.7

--- exercises/chapter3/mandelbrod/mandelbrod.go
+++ exercises/chapter3/mandelbrod/mandelbrod.go
@@ -18,13 +18,19 @@ const (
 	blue                   = 250
 )
 
-func Draw(w io.Writer) {
+func Draw(w io.Writer, f string) {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
-			m := mandelbroad(complex(x, y))
+			var m int
+			switch f {
+			case "mandelbroad", "":
+				m = mandelbroad(complex(x, y))
+			case "newton":
+				m = newton(complex(x, y))
+			}
 
 			col := color.NRGBA{R: uint8(red * m),
 				G: uint8(green * m), B: uint8(blue * m), A: 255}
@@ -42,6 +48,16 @@ func mandelbroad(c complex128) int {
 	return i
 }
 
+func newton(z complex128) int {
+	for i := 0; i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
+			return i
+		}
+	}
+	return 0
+}
+
 func main() {
-	Draw(os.Stdout)
+	Draw(os.Stdout, "newton")
 }
